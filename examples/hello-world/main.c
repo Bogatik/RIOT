af20906b6b5f4ab8a3bdf92eb10e550331b896f9
Student <bogatik-97@mail.ru>
2018-03-31 17:31:09 +0300

    Stasik

--- examples/hello-world/main.c
+++ examples/hello-world/main.c
@@ -23,14 +23,48 @@
 
 #include "periph/gpio.h"
 #include "rtctimers-millis.h"
+
+#include "thread.h"
+#include "shell_commands.h"
+#include "shell.h"
+
+#include "periph/pm.h"
+static kernel_pid_t process_pid ;
+static msg_t process_msg ; 
+
+#define MY_PROCESS_STACK_SIZE (1024)
+#define ENABLE_DEBUG (1)
+#include "debug.h"
+
+
 uint32_t milliseconds_last_press = 0 ;
+
+static void *process_thread(void *arg) {
+(void)arg;
+msg_t message ;
+gpio_set(GPIO_PIN(PORT_B, 0));
+while (1)
+ {
+	msg_receive(&message) ;
+	gpio_toggle(GPIO_PIN(PORT_B, 0));
+	if (gpio_read(GPIO_PIN(PORT_B, 0)) == 1)
+	{
+	DEBUG("Stasik privet %d\n", gpio_read(GPIO_PIN(PORT_B, 0)));
+	
+	}else 
+	
+	DEBUG("Stasik poka %d\n", gpio_read(GPIO_PIN(PORT_B, 0)));
+	}
+	return NULL;
+}
+
 
 static void btn_led_toggle(void* arg) {
 (void)arg;
 
-if ((rtctimers_millis_now() - milliseconds_last_press) > 100)
+if ((rtctimers_millis_now() - milliseconds_last_press) > 200)
 {
-gpio_toggle(GPIO_PIN(PORT_B, 0));
+msg_send(&process_msg, process_pid);
 milliseconds_last_press = rtctimers_millis_now();
 
 }
@@ -40,6 +74,9 @@ milliseconds_last_press = rtctimers_millis_now();
 
 int main(void)
 {
+	pm_init();
+	pm_prevent_sleep = 1;
+
 	rtctimers_millis_init();
     puts("Hello World!");
 
@@ -47,9 +84,13 @@ int main(void)
     printf("This board features a(n) %s MCU.\n", RIOT_MCU);
 
 	gpio_init (GPIO_PIN(PORT_B, 0), GPIO_OUT) ;
-	gpio_set (GPIO_PIN(PORT_B, 0)) ;
+	//gpio_set (GPIO_PIN(PORT_B, 0)) ;
 	gpio_init_int(GPIO_PIN(PORT_B, 1), GPIO_IN_PU, GPIO_FALLING, btn_led_toggle, NULL);
 	
-	
+	char stack[MY_PROCESS_STACK_SIZE] ; 
+	process_pid = thread_create(stack, MY_PROCESS_STACK_SIZE,
+				    THREAD_PRIORITY_MAIN -1, THREAD_CREATE_STACKTEST,
+				    process_thread, NULL, "our process") ;
+
     return 0;
 }
